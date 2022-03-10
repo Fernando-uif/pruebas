e1645bb3e3105d72910dc3e2c9656fca21c82799
Fernando <fst_96@hotmail.com>
2022-03-10 17:19:38 -0600

    funcin caracteres

---
+++ acentpsNorequerido/app.js
@@ -0,0 +1,15 @@
+const elemento = document.querySelector('.inpu1');
+console.log(elemento);
+
+const regCaracteres = ( etiqueta ) => {
+    // * Expresion regular para detecar si vienen caracteres especiales
+    const reg = /[^a-zA-Z\s]/g;
+
+    // * Validacion si viene el texto con algun caracter especial
+    // * Mandamos un aviso si incumple seguido a ello lo dejamos limpio
+    if(reg.test(etiqueta.value)){
+        window.alert('No debe de contener caracteres especiales');
+        etiqueta.value = '';
+        return;
+    }
+}
