a184de81a38e47cef4f48a1fa012168d4eec88c5
Fernando <fst_96@hotmail.com>
2022-03-11 11:35:32 -0600

    merge de spread

--- acentpsNorequerido/app.js
+++ acentpsNorequerido/app.js
@@ -3,7 +3,7 @@ console.log(elemento);
 
 const regCaracteres = ( etiqueta ) => {
     // * Expresion regular para detecar si vienen caracteres especiales
-    const reg = /[^a-zA-Z\s]/g;
+    const reg = /[^a-zA-Z\s\0-9]/g;
 
     // * Validacion si viene el texto con algun caracter especial
     // * Mandamos un aviso si incumple seguido a ello lo dejamos limpio
--- mergeArreglo/app.js
+++ mergeArreglo/app.js
@@ -44,6 +44,72 @@ let arr5 = [1,2,3,4,5,6,'hello'];
 
 let arreglo = ['palabra', 'palabra2'];
 let arreglo2 = ['palabra3','palabra4']
-console.log(...arreglo, ...arreglo2);
+
+
+// * -----------------------------------
+// * en arreglo se puede juntar
+let arreglo5 = ['elemento', 'elemento2'];
+let arreglo6 = ['elemento3', 'elemento4'];
+
+let resultanteArreglo = [...arreglo5, ...arreglo6];
+
+// * ---------------------------------------
+// * se puede juntar entre objetos
+let objeto = {
+    nombre:'fer',
+    apellido:'sosa',
+};
+let objeto2 = {
+    edad:1,
+    estatura:1_8,
+}
+
+let resultanteObjeto = {...objeto, objeto2};
+
+// * ---------------------------------------
+// ! dentro de arreglo no se puede hacer merge, de un arr y un obj
+let arregloOb1 = ['palabra1','palabra2'];
+let objArregl = {
+    nombre:'fer',
+    apellido:'sosa',
+}
+// let resultanteObjArr = [...arregloOb1, ...objArregl];
+// * -----------------------------------------
+// * Se puede combinar arreglo y objeto dentro e un objeto
+let arregloOb2 = ['palabra1','palabra2'];
+let objArregl2 = {
+    nombre:'fer',
+    apellido:'sosa',
+}
+let resultanteObjArr2 = { ...arregloOb2, ...objArregl2};
+// console.log(resultanteObjArr2);
+
+// * ---------------------------------------
+// * Se puede merge de dos arreglo dentro de un objeto
+let arregloObj3 = ['palabra','palabra2'];
+let objArregl3 = ['palabra3','palabra4'];
+
+let resultanteObjeArr3 = { ...arregloObj3, ...objArregl3};
+console.log(resultanteObjeArr3);
+
+// * ------------------------------------------
+// ! No se puede juntar dos objetos dentro de un arreglo
+let arregloObj4 = { nombre:'fer',apellido:'sosa'};
+let objArregl4 = { edad:0, estatura:1_80};
+
+// let resultanteObjeArr4 = [...arregloObj4, ...objArregl4];
+// console.log(resultanteObjeArr4);
+
+// * ---------------------------------------------
+// * Agregar un objeto desde un array sirve en react
+let arregloObj5 = [];
+let objArr5 = {
+    nombre:'fer',
+    apellido:'sosa'
+};
+let resultante11 = [...arregloObj5,objArr5];
+console.log(resultante11);
+
+
 
 
