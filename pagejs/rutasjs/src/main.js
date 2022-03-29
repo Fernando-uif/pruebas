00f86d9c758ccca9d77147d64789c5177f3e1da0
Fernando <fernando.sosa@grupobituaj.com.mx>
2022-03-29 12:46:45 -0600

    colocando pages , eliminar espacios y objetos

---
+++ pagejs/rutasjs/src/main.js
@@ -0,0 +1,23 @@
+"use strict";
+const rutas = (route) => {
+    switch (route) {
+        case '#/':
+            console.log('Estamos en el login');
+            break;
+        case '#/usuarios':
+            console.log('Estamos en usuario');
+            break;
+        case '#/login':
+            console.log('Estamos en login');
+            break;
+        case '#/home':
+            console.log('Estamos en home');
+            break;
+        default:
+            console.log('No se ha encontrado nada 404');
+            break;
+    }
+};
+window.addEventListener('hashchange', () => {
+    rutas(window.location.hash);
+});
--- retornoNumero/app.js
+++ retornoNumero/app.js
@@ -1,35 +1,39 @@
-function soloNumero (event) {
-
-    if (event.keyCode < 48 || event.keyCode > 57) (event.returnValue = false);
-   
-    
-
+function soloNumero(event) {
+  if (event.keyCode < 48 || event.keyCode > 57) event.returnValue = false;
 }
-function soloTexto ( event ){
-    if( !(event.charCode >= 65 && event.charCode <= 90  || event.keyCode >= 97 && event.keyCode <= 122 || event.charCode === 32)) event.returnValue = false;
+function soloTexto(event) {
+  if (
+    !(
+      (event.charCode >= 65 && event.charCode <= 90) ||
+      (event.keyCode >= 97 && event.keyCode <= 122) ||
+      event.charCode === 32
+    )
+  )
+    event.returnValue = false;
 }
-   
-   
 
-const inputTexto = document.querySelector('#inputTexto');
-console.log(inputTexto,'inputtexto');
+const inputTexto = document.querySelector("#inputTexto");
+console.log(inputTexto, "inputtexto");
 
-inputTexto.addEventListener('keypress',(event) =>{
-    if( !(event.charCode >= 65 && event.charCode <= 90  || event.keyCode >= 97 && event.keyCode <= 122 || event.charCode === 32)){
-        console.log('dentro condision');
-        
-        
-        event.returnValue = false;
-    }
-    console.log(event,'tenemos el evento');
-    console.log('algo');
-    
-    
-})
+inputTexto.addEventListener("keypress", (event) => {
+  if ( !( (event.charCode >= 65 && event.charCode <= 90) ||
+      (event.keyCode >= 97 && event.keyCode <= 122) ||
+      event.charCode === 32
+    )
+  ) {
+    console.log("dentro condision");
+
+    event.returnValue = false;
+  }
+  console.log(event, "tenemos el evento");
+  console.log("algo");
+});
 
 // 34 173
 
-function soloTextoCaracter ( event ){
-    if( !(event.charCode >= 34 && event.charCode <= 173 || event.charCode === 32)) event.returnValue = false;
-    
+function soloTextoCaracter(event) {
+  if (
+    !((event.charCode >= 34 && event.charCode <= 173) || event.charCode === 32)
+  )
+    event.returnValue = false;
 }
